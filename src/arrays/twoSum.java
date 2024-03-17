c2e761ac287fb803d1f09eac58b575e6bfa6508e
coder-saksham <sakshamtw23@gmail.com>
2024-03-17 23:00:18 +0530

    problem practiced on 17 mar

---
+++ src/arrays/twoSum.java
@@ -0,0 +1,28 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
