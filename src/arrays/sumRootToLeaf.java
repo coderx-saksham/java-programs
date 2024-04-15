aadef9dfeff2b36caa97c99728a55cfa66348246
coder-saksham <sakshamtw23@gmail.com>
2024-04-15 23:36:45 +0530

    problem practiced on 15 apr

---
+++ src/arrays/sumRootToLeaf.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class sumRootToLeaf {
+
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode node, int pathSum) {
+            if (node == null)
+                return 0;
+
+            pathSum = pathSum * 10 + node.val;
+
+            if (node.left == null && node.right == null)
+                return pathSum;
+
+            return dfs(node.left, pathSum) + dfs(node.right, pathSum);
+        }
+}
